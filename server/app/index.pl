544e7abc43d2c8598674a7622e9df960bda0ca0d
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-23 20:57:35 +0900

    DBと接続

--- server/app/index.pl
+++ server/app/index.pl
@@ -20,3 +20,12 @@ my $dbh = DBI->connect( $dsn, $user, $pass, {
     ShowErrorStatement => 1,
     AutoInactiveDestroy => 1
 })|| die $DBI::errstr;
+
+my $sth = $dbh->prepare("SELECT * FROM user");
+$sth->execute();
+while (my $ary_ref = $sth->fetchrow_arrayref) {
+  my ($id, $name) = @$ary_ref;
+  print "<h3>", $id, " ", $name, "</h3>\n";
+}
+$sth->finish;
+$dbh->disconnect;
