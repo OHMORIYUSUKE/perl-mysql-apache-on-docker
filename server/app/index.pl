c874b157cc22dc10a3a27c44c3f82e7a783a8de9
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-29 00:03:53 +0900

    pl を修正

--- server/app/index.pl
+++ server/app/index.pl
@@ -1,12 +1,9 @@
 #!/usr/bin/perl
 
-use utf8;
 use CGI;
 use strict;
 use warnings;
 use DBI;
-
-binmode STDOUT, ":encoding(Shift_JIS)";
 
 # データベース接続準備
 my $dsn = "dbi:mysql:database=fuel_dev;host=db;port=3306";
@@ -30,12 +27,14 @@ my $query = CGI -> new;
 
 my $ch_name = $query -> param('ch_name');
 my $cv_name = $query -> param('cv_name');
+my $ch_blood_type = $query -> param('ch_blood_type');
+my $type = $query -> param('type');
 
 #
 #-- --PRINT-- -- -- -
 #
 
-print "Content-Type: text/html;\n\n";
+print "Content-Type: text/html; charset=UTF-8;\n\n";
 print "<html lang='ja'>";
 print "<head><title>アイマス図鑑</title><link rel='stylesheet' href='style/main.css'></head>";
 print "<body>";
@@ -44,37 +43,68 @@ print "<div class='blocktext'>";
 
 print '<div class="container">';
 
-print "<div>";
+print '<FORM action="./" class="container">';
+
+print '<div style="margin: 10px">';
 print "<h5>アイドルの名前から検索</h5>";
-print '<FORM method="POST" action="./">';
 print '<LABEL>名前</LABEL><INPUT type="text" name="ch_name" value="', $ch_name, '">';
-print "<button>検索</button>";
-print "</FORM>";
 print "</div>";
 
-print "<div>";
+print '<div style="margin: 10px">';
 print "<h5>声優の名前から検索</h5>";
-print '<FORM method="POST" action="./">';
 print '<LABEL>名前</LABEL><INPUT type="text" name="cv_name" value="', $cv_name, '">';
-print "<button>検索</button>";
-print "</FORM>";
 print "</div>";
 
-print '<FORM method="POST" action="./">';
-print '<INPUT type="hidden" name="cv_name" value="%">';
-print '<INPUT type="hidden" name="ch_name" value="%">';
-print "<button>すべて表\示する</button>";
+print "</div>";
+
+print '<div class="container">';
+
+# SQL(血液型)
+my $sth = $dbh -> prepare("SELECT DISTINCT ch_blood_type FROM imas_characters");
+$sth -> execute();
+print '<div style="margin: 10px">';
+
+print '<label for="ch_blood_type">血液型</label>';
+print '<select id="ch_blood_type" name="ch_blood_type" size="4">';
+print "<option value=''>指定なし</option>";
+while (my $ary_ref = $sth -> fetchrow_arrayref) {
+    my($data) = @$ary_ref;
+    $data ne 0 && $data ne $ch_blood_type && print '<option value="', $data, '">', $data, "</option>";
+    $data eq $ch_blood_type && print '<option value="', $data, '" selected>', $data, "</option>";
+}
+print "</select>";
+
+
+$sth = $dbh -> prepare("SELECT DISTINCT type FROM imas_characters");
+$sth -> execute();
+print '<label for="type">グループ</label>';
+print '<select id="type" name="type" size="3">';
+print "<option value=''>指定なし</option>";
+while (my $ary_ref = $sth -> fetchrow_arrayref) {
+    my($data) = @$ary_ref;
+    $data != $type && print '<option value="', $data, '">', $data, "</option>";
+    $data == $type && print '<option value="', $data, '" selected>', $data, "</option>";
+}
+print "</select>";
+
+print "<button>検索</button>";
 print "</FORM>";
+
+print "</div>";
 
 print "</div>";
 
 
 $ch_name = "%".$ch_name."%";
 $cv_name = "%".$cv_name."%";
+$ch_blood_type = $ch_blood_type ? $ch_blood_type : "%";
+$type = $type ? $type : "%";
 if ($ch_name ne "") {
-    my $sth = $dbh -> prepare("SELECT * FROM imas_characters WHERE ch_name LIKE ? AND cv_name LIKE ?");
+    my $sth = $dbh -> prepare("SELECT * FROM imas_characters WHERE ch_name LIKE ? AND cv_name LIKE ? AND ch_blood_type LIKE ? AND type LIKE ?");
     $sth -> bind_param(1, $ch_name);
     $sth -> bind_param(2, $cv_name);
+    $sth -> bind_param(3, $ch_blood_type);
+    $sth -> bind_param(4, $type);
     $sth -> execute();
 
     while (my $ary_ref = $sth -> fetchrow_arrayref) {
@@ -84,9 +114,11 @@ if ($ch_name ne "") {
         $cv_name ? print "<p>　　<ruby><rb>", $cv_name, "<rb><rp>（</rp><rt>", $cv_name_ruby, "</rt><rp>）</rp></ruby>　", $cv_birth_month, "月", $cv_birth_day, "日生まれ", "　", $cv_gender == 1 ? "女性" : "男性", "</p>" : print "";
     }
 } else {
-    my $sth = $dbh -> prepare("SELECT * FROM imas_characters WHERE ch_name LIKE ? AND cv_name LIKE ?");
+    my $sth = $dbh -> prepare("SELECT * FROM imas_characters WHERE ch_name LIKE ? AND cv_name LIKE ? AND ch_blood_type LIKE ? AND type LIKE ?");
     $sth -> bind_param(1, $ch_name);
     $sth -> bind_param(2, $cv_name);
+    $sth -> bind_param(3, $ch_blood_type);
+    $sth -> bind_param(4, $type);
     $sth -> execute();
 
     while (my $ary_ref = $sth -> fetchrow_arrayref) {
